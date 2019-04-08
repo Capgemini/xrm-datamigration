295fd64f54cb726a80b0805cdd2894e1cd0dfcbd
Kriss Sulikowski <kriss.sulikowski@capgemini.com>
2019-04-08 16:01:32 +0100

    Example added

--- Capgemini.Xrm.Datamigration/Capgemini.Xrm.Datamigration.Examples/Program.cs
+++ Capgemini.Xrm.Datamigration/Capgemini.Xrm.Datamigration.Examples/Program.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using Capgemini.DataMigration.Core;
+using Capgemini.DataMigration.Resiliency.Polly;
+using Capgemini.Xrm.Datamigration.Examples.Properties;
+using Capgemini.Xrm.DataMigration.CrmStore.Config;
+using Capgemini.Xrm.DataMigration.Engine;
+using Capgemini.Xrm.DataMigration.Repositories;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Capgemini.Xrm.Datamigration.Examples
@@ -10,6 +18,31 @@ namespace Capgemini.Xrm.Datamigration.Examples
     {
         static void Main(string[] args)
         {
+            ExportDataXml(Settings.Default.CrmExportConnectionString);
+        }
+
+        static void ExportDataXml(string connectionString)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            var serviceClient = new CrmServiceClient(connectionString);
+
+            var exportConfig = new CrmExporterConfig()
+            {
+                BatchSize = 1000,
+                PageSize = 500,
+                FilePrefix = "EX0.1",
+                JsonFolderPath = "Extract",
+                OneEntityPerBatch = true,
+                SeperateFilesPerEntity = true,
+                TopCount = 10000
+            };
+
+            var entityRepo = new EntityRepository(serviceClient, new ServiceRetryExecutor());
+
+            var fileExporter = new CrmFileDataExporter(new ConsoleLogger(), entityRepo, exportConfig, cancellationTokenSource.Token);
+
+            fileExporter.MigrateData();
         }
     }
 }
