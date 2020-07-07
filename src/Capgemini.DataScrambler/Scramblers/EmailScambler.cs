46bb2b7c13e10ad66ef419fad6c8c7f636c64eec
David Powell <david.powell@capgemini.com>
2020-07-07 17:30:28 +0100

    Removed the reference to Capgemini.DataScrambler package and embedded the Scambler code into the solution

---
+++ src/Capgemini.DataScrambler/Scramblers/EmailScambler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.DataScrambler.Scramblers
+{
+    public class EmailScambler : StringScrambler
+    {
+        public EmailScambler() : base()
+        {
+
+        }
+
+        public override string Scramble(string input, int min, int max)
+        {
+            string[] partsOfEmail = input.Split('@');
+            ValidateEmail(partsOfEmail);
+
+            partsOfEmail[0] = ScrambleString(partsOfEmail[0], min, max);
+            partsOfEmail[1] = ScrambleString(partsOfEmail[1], min, max);
+            return String.Join("@", partsOfEmail);
+        }
+
+        private void ValidateEmail(string[] partsOfEmail)
+        {
+            if (partsOfEmail.Length != 2)
+            {
+                throw new Exception("EmailScambler: This is not a valid email");
+            }
+        }
+    }
+}
---
+++ src/Capgemini.DataScrambler/Scramblers/StringScrambler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini.DataScrambler.Scramblers
+{
+    public class StringScrambler : IScrambler<string>
+    {
+        Random RandomGenerator { get; }
+        const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public StringScrambler()
+        {
+            RandomGenerator = new Random();
+        }
+
+        public virtual string Scramble(string input, int min, int max)
+        {
+            return ScrambleString(input, min, max);
+        }
+
+        protected string ScrambleString(string input, int min, int max)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in input)
+            {
+                int randomNumber = RandomGenerator.Next(0, Chars.Length);
+                char newCharacter = Chars[randomNumber];
+                sb.Append(newCharacter);
+            }
+
+            string outputString = sb.ToString();
+            if (input == outputString)
+            {
+                outputString = Scramble(input, min, max);
+            }
+
+            return outputString;
+        }
+    }
+}
