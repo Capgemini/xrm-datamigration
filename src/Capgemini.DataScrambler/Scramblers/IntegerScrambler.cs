31605614457d8d0d27846e9ff71e89d0b7ef44ff
David Powell <david.powell@capgemini.com>
2020-07-29 21:23:12 +0100

    Added a Static Random object that is used by all Scramblers to stop the same replacement values being created

--- src/Capgemini.DataScrambler/Scramblers/IntegerScrambler.cs
+++ src/Capgemini.DataScrambler/Scramblers/IntegerScrambler.cs
@@ -7,21 +7,18 @@ using System.Threading.Tasks;
 namespace Capgemini.DataScrambler.Scramblers
 {
     public class IntegerScrambler : IScrambler<int>
-    {
-        Random RandomGenerator { get; }
-        
+    {        
         public IntegerScrambler()
         {
-            RandomGenerator = new Random();
         }
 
         public int Scramble(int input, int min, int max)
         {
 
-            int randomNumber = RandomGenerator.Next(min, max);
+            int randomNumber = RandomGenerator.GetRandom.Next(min, max);
             while(randomNumber == input)
             {
-                randomNumber = RandomGenerator.Next(min, max);
+                randomNumber = RandomGenerator.GetRandom.Next(min, max);
             }
 
             return randomNumber;
