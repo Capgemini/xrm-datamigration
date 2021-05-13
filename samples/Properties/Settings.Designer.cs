a1fb4ff9e6b2f634d83f744c60ed2319657cf07c
Kriss <ksulikowski@hotmail.com>
2021-05-13 16:20:33 +0100

    Add multithreading support to the demo console app (#49)
    * Adding multithreading
    
    * Change default thread to 1
    
    * removing ProductPriceLevel example
    
    * Adding Clone() handling

--- samples/Properties/Settings.Designer.cs
+++ samples/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Capgemini.Xrm.Datamigration.Examples.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -58,6 +58,15 @@ namespace Capgemini.Xrm.Datamigration.Examples.Properties {
         public string CrmImportConnectionString {
             get {
                 return ((string)(this["CrmImportConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public int ThreadCount {
+            get {
+                return ((int)(this["ThreadCount"]));
             }
         }
     }
