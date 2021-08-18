Capgemini/xrm-datamigration

commit b2381c76a4feeb7cd275aed6b0fb5885177fe107

perf: cache alias mapping results (#59)
Caches GUIDS for alias mappings to cut down on repeat API requests.

This relatively minor fix brings down an import job that takes us 25 minutes to a more reasonable 5 minutes. It's mapping 6000 records against a relatively small dataset (systemusers). All reference data is assigned to the same mapped user, so it makes 6000 requests to get the same user. This commit ensures that only one request is ever made for that same set of aliased values.

src/Capgemini.Xrm.DataMigration.Engine/MappingRules/MappingAliasedValueRule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Capgemini.Xrm.DataMigration.Core;
@@ -12,23 +13,77 @@ namespace Capgemini.Xrm.DataMigration.Engine.MappingRules
     public class MappingAliasedValueRule : IMappingRule
     {
         private readonly IEntityRepository entityRepository;
+        private readonly Dictionary<AliasMapCacheKey, Guid> cache;
 
         public MappingAliasedValueRule(IEntityRepository entityRepository)
         {
             this.entityRepository = entityRepository;
+            this.cache = new Dictionary<AliasMapCacheKey, Guid>();
         }
 
         public bool ProcessImport(string aliasedAttributeName, List<AliasedValue> values, out object replacementValue)
         {
-            string entName = values.Select(p => p.EntityLogicalName).Distinct().Single();
+            var entityName = values.Select(p => p.EntityLogicalName).Distinct().Single();
+            var attributeNames = values.Select(p => p.AttributeLogicalName).ToArray();
+            var attributeValues = values.Select(p => p.Value).ToArray();
 
-            List<string> attrNames = values.Select(p => p.AttributeLogicalName).ToList();
-
-            List<object> attrValues = values.Select(p => p.Value).ToList();
-
-            replacementValue = entityRepository.GetGuidForMapping(entName, attrNames.ToArray(), attrValues.ToArray());
+            var cacheKey = new AliasMapCacheKey(entityName, attributeNames, attributeValues);
+            if (cache.TryGetValue(cacheKey, out Guid cachedValue))
+            {
+                replacementValue = cachedValue;
+            }
+            else
+            {
+                replacementValue = entityRepository.GetGuidForMapping(entityName, attributeNames, attributeValues);
+                cache.Add(cacheKey, (Guid)replacementValue);
+            }
 
             return (Guid)replacementValue != Guid.Empty;
+        }
+
+        private class AliasMapCacheKey
+        {
+            public AliasMapCacheKey(string entityName, IEnumerable<string> attributeNames, IEnumerable<object> attributeValues)
+            {
+                this.EntityName = entityName;
+                this.AttributeNames = attributeNames;
+                this.AttributeValues = attributeValues;
+            }
+
+            public string EntityName { get; private set; }
+
+            public IEnumerable<string> AttributeNames { get; private set; }
+
+            public IEnumerable<object> AttributeValues { get; private set; }
+
+            public override bool Equals(object obj)
+            {
+                if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+                {
+                    return false;
+                }
+
+                var other = (AliasMapCacheKey)obj;
+
+                return
+                    this.EntityName == other.EntityName &&
+                    this.AttributeNames.SequenceEqual(other.AttributeNames) &&
+                    this.AttributeValues.SequenceEqual(other.AttributeValues);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    int hash = 17;
+
+                    hash = (hash * 23) + this.EntityName.GetHashCode();
+                    hash = (hash * 23) + ((IStructuralEquatable)this.AttributeNames).GetHashCode(EqualityComparer<string>.Default);
+                    hash = (hash * 23) + ((IStructuralEquatable)this.AttributeValues).GetHashCode(EqualityComparer<object>.Default);
+
+                    return hash;
+                }
+            }
         }
     }
 }

src/Capgemini.Xrm.DataMigration.Engine/DataProcessors/MapEntityProcessor.cs
@@ -52,8 +52,8 @@ namespace Capgemini.Xrm.DataMigration.Engine.DataProcessors
                 {
                     entitySpecificRules = new List<IMappingRule>()
                     {
-                new BusinessUnitRootRule(targetBusinessUnitId),
-                new MappingAliasedValueRule(targetRepo)
+                        new BusinessUnitRootRule(targetBusinessUnitId),
+                        new MappingAliasedValueRule(targetRepo)
                     };
                 }
 

tests/Capgemini.Xrm.DataMigration.Engine.Tests.Unit/MappingRules/MappingAliasedValueRuleTests.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using Capgemini.DataMigration.Core.Tests.Base;
 using Capgemini.Xrm.DataMigration.Engine.MappingRules;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Xrm.Sdk;
+using Moq;
 
 namespace Capgemini.Xrm.DataMigration.Engine.Tests.Unit.MappingRules
 {
@@ -45,6 +47,34 @@ namespace Capgemini.Xrm.DataMigration.Engine.Tests.Unit.MappingRules
 
             actual.Should().BeFalse();
             replacementValue.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void ProcessImportDuplicateAliasMappingQueriesAreCached()
+        {
+            string aliasedAttributeName = "accountid";
+            var firstValues = new List<AliasedValue>()
+            {
+                new AliasedValue("account", "name", "Test Account"),
+            };
+            var secondValues = new List<AliasedValue>()
+            {
+                new AliasedValue("account", "name", "Test Account"),
+            };
+            var returnedGuid = Guid.NewGuid();
+            MockEntityRepo
+                .Setup(r => r.GetGuidForMapping(It.IsAny<string>(), It.IsAny<string[]>(), It.IsAny<object[]>()))
+                .Returns(returnedGuid);
+
+            systemUnderTest.ProcessImport(aliasedAttributeName, firstValues, out object replacementValue);
+            systemUnderTest.ProcessImport(aliasedAttributeName, secondValues, out replacementValue);
+
+            MockEntityRepo.Verify(
+                r => r.GetGuidForMapping(It.IsAny<string>(), It.IsAny<string[]>(), It.IsAny<object[]>()),
+                Times.AtMostOnce(),
+                "Expected alias mappings to be cached but they were not.");
+
+            replacementValue.Should().BeEquivalentTo(returnedGuid);
         }
     }
 }

commit 11271baf853453b9ab2dcd320be73c5345060c4f

fix: unable to alias map pass one references
The caching applied in https://github.com/Capgemini/xrm-datamigration/commit/b2381c76a4feeb7cd275aed6b0fb5885177fe107 introduced a potentially breaking change for data imports where the alias mapping caches an empty GUID in pass zero for a record that gets created in pass zero. Pass one would then not be able to map to these records as a result of the cached empty GUID.

This change reverts that breaking change at the cost of import failure scenarios being about as performant as they were previously.

src/Capgemini.Xrm.DataMigration.Engine/MappingRules/MappingAliasedValueRule.cs
@@ -35,7 +35,11 @@ namespace Capgemini.Xrm.DataMigration.Engine.MappingRules
             else
             {
                 replacementValue = entityRepository.GetGuidForMapping(entityName, attributeNames, attributeValues);
-                cache.Add(cacheKey, (Guid)replacementValue);
+
+                if ((Guid)replacementValue != Guid.Empty)
+                {
+                    cache.Add(cacheKey, (Guid)replacementValue);
+                }
             }
 
             return (Guid)replacementValue != Guid.Empty;