04900ad3834b164a48fd20a8ce5cc4d8c194a521
Tom Ashworth <tom.ashworth@capgemini.com>
2023-01-13 16:02:45 +0000

    remove versions

--- src/Capgemini.Xrm.DataMigration.Cli/Properties/AssemblyInfo.cs
+++ src/Capgemini.Xrm.DataMigration.Cli/Properties/AssemblyInfo.cs
@@ -8,3 +8,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ff1caa60-e675-450b-bc29-a03930e33cfd")]
+[assembly: AssemblyVersion("3.1.26.0")]
+[assembly: AssemblyFileVersion("3.1.26.0")]
+[assembly: AssemblyInformationalVersion("3.1.26-tech-project-upgrades.1+Branch.tech-project-upgrades.Sha.04b2ae15be99ca9ae40406aa15ec143a4f748ed7")]
