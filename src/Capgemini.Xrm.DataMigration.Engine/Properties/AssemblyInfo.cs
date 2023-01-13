a8c33b22a24ea5607b4c8e57ff0c6f59ce0ed832
Tom Ashworth <tom.ashworth@capgemini.com>
2023-01-13 22:38:06 +0000

    tech: upgrade project to VS2019 and pack symbols for nuget (#102)
    * tech: upgrade project to VS2019 and pack symbols for nuget

--- src/Capgemini.Xrm.DataMigration.Engine/Properties/AssemblyInfo.cs
+++ src/Capgemini.Xrm.DataMigration.Engine/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Capgemini Data Migration Engine")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Capgemini")]
-[assembly: AssemblyProduct("Capgemini.Xrm.DataMigration.Engine")]
-[assembly: AssemblyCopyright("Copyright © Capgemini Corporation 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0fb0162d-0b31-4c5b-a657-f4f3579778ec")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
--- tests/Capgemini.Xrm.DataMigration.Tests.Integration/Properties/AssemblyInfo.cs
+++ tests/Capgemini.Xrm.DataMigration.Tests.Integration/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Capgemini.Xrm.DataMigration.Tests.Integration")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Capgemini.Xrm.DataMigration.Tests.Integration")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("42785abf-1469-4788-bf62-d3e14264f5f7")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
