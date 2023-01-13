fd23f7b9a499e4b3e7ecb6d54209a43a4702c52a
Tom Ashworth <tom.ashworth@capgemini.com>
2023-01-13 22:38:06 +0000

    tech: upgrade project to VS2019 and pack symbols for nuget (#102)
    * tech: upgrade project to VS2019 and pack symbols for nuget

--- tests/Capgemini.Xrm.DataMigration.FileStore.Tests.Unit/Properties/AssemblyInfo.cs
+++ tests/Capgemini.Xrm.DataMigration.FileStore.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Capgemini.Xrm.DataMigration.FileStore.Tests.Unit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Capgemini.Xrm.DataMigration.FileStore.Tests.Unit")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e3f232aa-fdbc-4716-a233-87db51608ed8")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
