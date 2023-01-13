04b2ae15be99ca9ae40406aa15ec143a4f748ed7
Tom Ashworth <tom.ashworth@capgemini.com>
2023-01-13 12:15:35 +0000

    tech: upgrade project to VS2019 and pack symbols for nuget

--- src/Capgemini.Xrm.DataMigration.FileStore/Properties/AssemblyInfo.cs
+++ src/Capgemini.Xrm.DataMigration.FileStore/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Capgemini.Xrm.DataMigration.FileStore")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Capgemini")]
-[assembly: AssemblyProduct("Capgemini.Xrm.DataMigration.FileStore")]
-[assembly: AssemblyCopyright("Copyright © Capgemini Corporation 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("15394533-d8ae-4f0f-9263-be9e94f12336")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
