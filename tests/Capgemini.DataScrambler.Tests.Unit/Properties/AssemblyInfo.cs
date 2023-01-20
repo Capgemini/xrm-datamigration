eec1030132b473df92e9ad6782f0864d888edcee
Tom Ashworth <tom.ashworth@capgemini.com>
2023-01-20 15:40:19 +0000

    Revert "tech: upgrade project to VS2019 and pack symbols for nuget (#102)"
    This reverts commit a8c33b22a24ea5607b4c8e57ff0c6f59ce0ed832.

--- tests/Capgemini.DataScrambler.Tests.Unit/Properties/AssemblyInfo.cs
+++ tests/Capgemini.DataScrambler.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,7 +1,20 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Capgemini.DataScrambler.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Capgemini.DataScrambler.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("768db632-8bb1-4dcf-b122-19f7feafa233")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
