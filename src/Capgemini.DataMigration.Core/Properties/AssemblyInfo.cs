7fecf5a9689754b79da54b54b3c1daee75a1b4e2
Tom Ashworth <tom.ashworth@capgemini.com>
2023-01-20 16:43:12 +0000

    ci: revert to old style project with symbols support (#106)
    * ci: nuget pack
    
    * ci: remove dotnet pack
    
    * ci: correct pack command
    
    * ci: install nuget version 5
    
    * ci: fix indentation
    
    * ci: update nuget paths
    
    * ci: nuget version
    
    * Ci: changed configuration to debug
    
    * ci: use "pack" command
    
    * ci: includeReferencedProjects
    
    * Revert "tech: upgrade project to VS2019 and pack symbols for nuget (#102)"
    
    This reverts commit fd23f7b9a499e4b3e7ecb6d54209a43a4702c52a.
    
    * ci: custom nuget command
    
    * docs: update nuspec
    
    * ci: license type
    
    Co-authored-by: Dan Roche <52405038+danroche10@users.noreply.github.com>

--- src/Capgemini.DataMigration.Core/Properties/AssemblyInfo.cs
+++ src/Capgemini.DataMigration.Core/Properties/AssemblyInfo.cs
@@ -1,6 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Capgemini.DataMigration.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Capgemini")]
+[assembly: AssemblyProduct("Capgemini.DataMigration.Core")]
+[assembly: AssemblyCopyright("Copyright © Capgemini Corporation 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -8,3 +20,16 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c8b84af3-31dd-44ae-80cd-13ab60549f7c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
--- tests/Capgemini.DataMigration.Core.Tests.Unit/Properties/AssemblyInfo.cs
+++ tests/Capgemini.DataMigration.Core.Tests.Unit/Properties/AssemblyInfo.cs
@@ -1,4 +1,17 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Capgemini.DataMigration.Core.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Capgemini.DataMigration.Core.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9ce399d0-687e-4096-bf2e-63735c446f21")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
